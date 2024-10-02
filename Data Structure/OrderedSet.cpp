d621e845fbdc40a7634e4a7b3f951f4eeb4baf91
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2024-10-03 00:45:23 +0600

    Update OrderedSet.cpp

--- Data Structure/OrderedSet.cpp
+++ Data Structure/OrderedSet.cpp
@@ -3,3 +3,13 @@
 using namespace __gnu_pbds;
 template <typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+// find_by_order(k): It returns to an iterator to the kth element (counting from zero) in the set in O(logn) time.To find the first element k must be zero.
+//   Let us assume we have a set s : {1, 5, 6, 17, 88}, then :  
+//   *(s.find_by_order(2)) : 3rd element in the set i.e. 6
+//   *(s.find_by_order(4)) : 5th element in the set i.e. 88
+// order_of_key(k) : It returns to the number of items that are strictly smaller than our item k in O(logn) time.
+//   Let us assume we have a set s : {1, 5, 6, 17, 88}, then :
+//   s.order_of_key(6) : Count of elements strictly smaller than 6 is 2.
+//   s.order_of_key(25) : Count of elements strictly smaller than 25 is 4.
