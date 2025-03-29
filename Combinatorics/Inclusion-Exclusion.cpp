13daeec37603a999c5be79f8959fb6766036489e
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-03-29 16:05:54 +0600

    Update Inclusion-Exclusion.cpp

--- Combinatorics/Inclusion-Exclusion.cpp
+++ Combinatorics/Inclusion-Exclusion.cpp
@@ -41,3 +41,4 @@ void solve() {
 //https://cses.fi/problemset/task/2185/
 //https://www.spoj.com/problems/MOMOS/en/
 //https://leetcode.com/problems/ugly-number-iii/submissions/1587470824/
+//https://atcoder.jp/contests/abc246/tasks/abc246_f
