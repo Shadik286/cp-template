eab01c5a22726340f0ed4e4a7938a89d3096da22
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-04-12 13:27:16 +0600

    Create Sieve.cpp

---
+++ math/Sieve.cpp
@@ -0,0 +1,20 @@
+vector<int>pms;
+vector<int>fnd(N);
+void prec() { 
+    for(int i = 2; i < N; i++) {
+        if(fnd[i]) continue;
+        fnd[i] = i; pms.push_back(i);
+        for(int j = i + i; j < N; j += i) {
+            fnd[j] = i; 
+        } 
+    } 
+} 
+// Below function returns the primes that divides number n. (Filters out the primes that are not divisors of n)
+set<int>dv(int n) { 
+    set<int>tmp;  
+    while(n > 1) {  
+        tmp.insert(fnd[n]); 
+        n /= fnd[n];
+    }
+    return tmp;
+}
