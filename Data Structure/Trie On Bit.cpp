66a154d986b970af2892aa99133b2492cb89e075
Shadik Rahman <104418894+Shadik286@users.noreply.github.com>
2025-05-19 01:31:49 +0600

    Update Trie On Bit.cpp

--- Data Structure/Trie On Bit.cpp
+++ Data Structure/Trie On Bit.cpp
@@ -1,9 +1,10 @@
+struct trie{ 
 
-struct trie{
-
+    int bt = 31;
     struct node {
         node* childs[2];
-        bool ends = false;
+        bool ends = false; 
+        int cnt = 0;
     }; 
 
     node* root;
@@ -13,19 +14,20 @@ struct trie{
 
     void insert(int val) {
         node* cur = root; 
-        for(int i = 30; i >= 0; i--) {
+        for(int i = bt; i >= 0; i--) {
             int bt = val >> i & 1;
             if(cur->childs[bt] == NULL) {
                 cur->childs[bt] = new node();
             } 
-            cur = cur->childs[bt];
+            cur = cur->childs[bt]; 
+            cur->cnt++;
         }
         cur->ends = true;
     } 
 
     bool search(int val) {
         node* cur = root;
-        for(int i = 30; i >= 0; i--) {
+        for(int i = bt; i >= 0; i--) {
             int bt = val >> i & 1;
             if(cur->childs[bt] == NULL) {
                 return false;
@@ -34,6 +36,34 @@ struct trie{
         }
         return cur->ends;
     } 
+
+    int get_max(int x) { // returns maximum of val ^ x
+        node* cur = root;
+        int ans = 0;
+        for (int i = bt; i >= 0; i--) {
+            int k = x >> i & 1; 
+            // Min er laghi !k er jaygay k and k er jaygay !k
+            if (cur -> childs[!k] and cur->childs[!k]->cnt) cur = cur -> childs[!k], ans <<= 1, ans++;
+            else cur = cur -> childs[k], ans <<= 1;
+
+            // For min
+            // if (cur -> childs[k] and cur.childs[k]->cnt) cur = cur -> childs[k], ans <<= 1;
+            // else cur = cur -> childs[!k], ans <<= 1, ans++;
+
+        }
+        return ans;
+    }
+    int get_min(int x) { // returns minimum of val ^ x
+        node* cur = root;
+        int ans = 0;
+        for (int i = bt; i >= 0; i--) {
+            int k = x >> i & 1;
+            if (cur -> childs[k]) cur = cur -> childs[k], ans <<= 1;
+            else cur = cur -> childs[!k], ans <<= 1, ans++;
+        }
+        return ans;
+    }
+
     void deleteTrie() {
         deleteTrie(root);
     }
@@ -50,9 +80,10 @@ struct trie{
     // Deletes a single element (Not Completed, the nodes are still there).
     void del(int val) {
         node* cur = root;
-        for(int i = 30; i >= 0; i--) {
+        for(int i = bt; i >= 0; i--) {
             int bt = val >> i & 1;
-            cur = cur->childs[bt];
+            cur = cur->childs[bt]; 
+            cur->cnt--;
         }
         cur->ends = false;
     }
@@ -62,11 +93,17 @@ struct trie{
 void solve() {
 
     trie* tr = new trie();
-
-    tr->insert(1); 
-    tr->insert(3);
-    tr->insert(2);
-    tr->del(1);
-    cout << tr->search(2);
+    tr->insert(0);
+    int n; cin >> n; 
+    for(int i = 0; i < n; i++) {
+        char c; cin >> c;
+        int t; cin >> t;
+        if(c == '+') tr->insert(t);
+        else if(c == '-') {
+            tr->del(t);
+        } else {
+            cout << tr->get_max(t) << '\n';
+        }
+    }
 
 }
